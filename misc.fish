e495d92a553fbf690940bae7629c002fc75a8397
Ben Moskovitz <ben@mosk.nz>
2018-02-22 16:08:40 +1300

    Fix aliases in misc.fish

--- misc.fish
+++ misc.fish
@@ -1,17 +1,16 @@
-alias nvm 'bass source ~/.nvm/nvm.sh --no-use ";" nvm $argv' # Use nvm because it by default doesn't work with fish
-alias psf 'pgrep $argv' # Find a running process
-alias dit 'docker exec -it $argv'
-alias dr 'docker run $argv'
-alias db 'docker build $argv'
-alias sagi 'sudo apt-get install -y $argv[1]'
-alias mkcd 'mkdir -p $argv[1] ;and cd $argv[1]'
-alias bi 'brew install $argv[1]'
-alias bs 'brew services $argv'
-alias code 'code-insiders $argv'
-alias et 'e t --reporter dot $argv'
+alias nvm 'bass source ~/.nvm/nvm.sh --no-use ";" nvm' # Use nvm because it by default doesn't work with fish
+alias psf 'pgrep' # Find a running process
+alias dit 'docker exec -it'
+alias dr 'docker run'
+alias db 'docker build'
+alias sagi 'sudo apt-get install -y'
+alias bi 'brew install'
+alias bs 'brew services'
+alias code 'code-insiders'
+alias et 'e t --reporter dot'
 alias bsl 'brew services list'
-alias p 'python3 $argv' # Typing is hard ok
-alias e 'nvm use 5.7.0 ;and ember $argv' # Switch to node 5.7.0, which I use a lot, then do something ember-related
+alias p 'python3' # Typing is hard ok
+alias e 'nvm use 5.7.0 ;and ember' # Switch to node 5.7.0, which I use a lot, then do something ember-related
 alias es 'e serve'
 alias quit 'exit' # 'quit' is significantly easier to type than 'exit'. I am a very lazy person
 alias gcat './gradlew clean connectedAndroidTest'
