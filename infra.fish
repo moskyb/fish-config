e7058f9ca8e685211f2d684485a802a4af3cb89d
Ben Moskovitz <ben@mosk.nz>
2020-07-07 10:48:07 +1200

    Add kubernetes utils

--- infra.fish
+++ infra.fish
@@ -7,24 +7,46 @@ abbr kc 'kubectl'
 abbr kcf 'kubectl -n feed-tracking'
 
 function klog
-  kubectl -n feed-tracking get pods | grep $argv[1] | awk '{print $1}' | xargs -n 1 kubectl -n feed-tracking logs $argv[2..-1]
+  kubectl get pods | grep $argv[1] | awk '{print $1}' | xargs -n 1 kubectl logs $argv[2..-1]
 end
 
 function kcl
   if count $argv > /dev/null
     set -gx KUBECONFIG ~/.kube/config_$argv[1]
-    set arn (kubectl config current-context)
-    echo -e "Set cluster to \e[34m$argv[1]\e[39m, ARN: \e[32m$arn"
-  else
-    set kenv (kubectl config current-context | grep -oe '\/.*' | sed 's|\/||')
-    echo -e "Currently working in the \e[34m$kenv\e[39m cluster"
   end
+  set kenv (kubectl config current-context | grep -oe '/.*' | sed 's|\/||')
+  set kns (kubectl config view -o json | jq .contexts[0].context.namespace -r)
+  set krole (rg -o "arn:aws:iam::680305091011:role/.*" $KUBECONFIG)
+  echo -e "Currently working in the \e[34m$kenv\e[39m cluster, \e[32m$kns\e[39m namespace, \e[35m$krole\e[39m role"
 end
 
 function volrm
   docker volume ls | grep $argv[1] | awk '{print $2}' | xargs docker volume rm
 end
 
+function kgetalland
+  kubectl get $argv[2] | grep $argv[3] | awk '{print $1}' | xargs kubectl $argv[1] $argv[2]
+end
+
 function kgetand
-  kubectl -n feed-tracking get $argv[2] | grep $argv[3] | awk '{print $1}' | head -n 1 | xargs kubectl -n feed-tracking $argv[1] $argv[2]
+  kubectl get $argv[2] | grep $argv[3] | awk '{print $1}' | head -n 1 | xargs kubectl $argv[1] $argv[2]
+end
+
+function ksetns
+  kubectl config set-context --current --namespace=$argv
+end
+
+function krole
+  if ! count $KUBECONFIG > /dev/null
+    echo "You don't have a \$KUBECONFIG set, try running kcl"
+    return
+  end
+  if ! count $argv > /dev/null
+    rg -oN --color never "arn:aws:iam::680305091011:role/.*" $KUBECONFIG
+    return
+  end
+  set kenv (kubectl config current-context | grep -oe '/.*' | sed 's|\/||')
+  set role "$kenv-kubernetes-$argv[1]-role"
+  sed -E -i '' "s|(arn:aws:iam::680305091011:role\/).*|\1$role|g" $KUBECONFIG
+  echo -e "Set role to \e[35m$role\e[39m"
 end
