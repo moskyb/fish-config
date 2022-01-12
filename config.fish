0a45aa1836ec25f152ef727d08054c20e469c203
Ben Moskovitz <ben@mosk.nz>
2022-01-12 13:24:58 +1300

    Move aws-vault completions into completions folder

--- config.fish
+++ config.fish
@@ -2,32 +2,6 @@
 set fish_greeting
 set PATH $PATH $GOPATH/bin
 
-if status --is-interactive
-  complete -ec aws-vault
-
-  # switch based on seeing a `--`
-  complete -c aws-vault -n 'not __fish_aws_vault_is_commandline' -xa '(__fish_aws_vault_complete_arg)'
-  complete -c aws-vault -n '__fish_aws_vault_is_commandline' -xa '(__fish_aws_vault_complete_commandline)'
-
-  function __fish_aws_vault_is_commandline
-    string match -q -r '^--$' -- (commandline -opc)
-  end
-
-  function __fish_aws_vault_complete_arg
-    set -l parts (commandline -opc)
-    set -e parts[1]
-
-    aws-vault --completion-bash $parts
-  end
-
-  function __fish_aws_vault_complete_commandline
-    set -l parts (string split --max 1 '--' -- (commandline -pc))
-
-    complete "-C$parts[2]"
-  end
-end
-
-
 source ~/.config/fish/fiddle.fish # Diddling with this file
 source ~/.config/fish/ruby.fish # Ruby-related stuffs
 source ~/.config/fish/git.fish # Many and varied git shortcuts
