994cef197efdec16a79d21ce0b73f3a31a696824
Ben Moskovitz <ben@mosk.nz>
2025-07-31 09:50:54 +1000

    Add orbstack shims

--- config.fish
+++ config.fish
@@ -45,3 +45,7 @@ fish_add_path "$HOME/.local/bin"
 fish_add_path "$HOME/.cargo/bin"
 
 kubectl completion fish | source
+
+# Added by OrbStack: command-line tools and integration
+# This won't be added again if you remove it.
+source ~/.orbstack/shell/init.fish 2>/dev/null || :
